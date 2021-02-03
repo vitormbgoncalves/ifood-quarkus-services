608362e15871befd5e940de3ea1949bbb100bdd6
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-02-02 21:04:35 -0300

    Testando endpoints reativos

---
+++ marketplace/src/test/java/com/github/vitormbgoncalves/ifood/PratoResourceTest.java
@@ -0,0 +1,20 @@
+package com.github.vitormbgoncalves.ifood;
+
+
+import static io.restassured.RestAssured.given;
+import org.junit.jupiter.api.Test;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class PratoResourceTest {
+
+  @Test
+  public void testBuscasPratos() {
+
+    String body = given()
+        .when().get("/pratos")
+        .then()
+        .statusCode(200).extract().asString();
+    System.out.println(body);
+  }
+}
