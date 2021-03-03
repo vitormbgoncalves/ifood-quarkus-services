78cf2d785588a931d73dd50d16c75b342c9585c1
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-03-03 10:19:58 -0300

    CLI com Picocli

--- command-mode/src/main/java/com/github/vitormbgoncalves/ifood/cli/MainDoQuarkus.java
+++ command-mode/src/main/java/com/github/vitormbgoncalves/ifood/cli/MainDoQuarkus.java
@@ -1,19 +1,14 @@
 package com.github.vitormbgoncalves.ifood.cli;
 
-import io.quarkus.runtime.Quarkus;
-import io.quarkus.runtime.QuarkusApplication;
-import io.quarkus.runtime.annotations.QuarkusMain;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
+import io.quarkus.picocli.runtime.annotations.TopCommand;
+import picocli.CommandLine;
 
-import javax.inject.Inject;
-import java.util.Arrays;
-import java.util.stream.Stream;
+//@QuarkusMain
+@TopCommand
+@CommandLine.Command(mixinStandardHelpOptions = true, subcommands = { Ola.class, Tchau.class })
+public class MainDoQuarkus  {
 
-@QuarkusMain
-public class MainDoQuarkus implements QuarkusApplication {
-
-  @Inject
+/*  @Inject
   @Channel("parametros")
   Emitter<String> emiter;
 
@@ -23,6 +18,30 @@ public class MainDoQuarkus implements QuarkusApplication {
     System.out.println("MainDoQuarkus.tun() " + Arrays.toString(args));
     Quarkus.waitForExit();
     return 0;
+  }*/
+}
+
+@CommandLine.Command(name = "Ola", description = "Imprime nome do usuario")
+class Ola implements Runnable {
+
+  @CommandLine.Option(names = { "-n", "--name" }, description = "Nome completo do usuario", defaultValue = "Vitor Goncalves")
+  String nome;
+
+  @Override
+  public void run() {
+    System.out.println("Ola.run()" + nome);
+  }
+}
+
+@CommandLine.Command(name = "tchau", description = "Imprime tchau e o nome do usuario")
+class Tchau implements Runnable {
+
+  @CommandLine.Option(names = { "-n", "--name" }, description = "Nome completo do usuario", required = true)
+  String nome;
+
+  @Override
+  public void run() {
+    System.out.println("Tchau.run()" + nome);
   }
 }
 
