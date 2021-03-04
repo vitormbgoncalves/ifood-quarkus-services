e569e919f3765e1f6133a3b8faa546eb768e9ff8
vitormbgoncalves <vitor@innovativetecnologia.com.br>
2021-03-04 10:55:34 -0300

    Deploy no Minikube

---
+++ kubernetes/src/test/java/com/github/vitormbgoncalves/ifood/kubernetes/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.vitormbgoncalves.ifood.kubernetes;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ kubernetes/src/test/java/com/github/vitormbgoncalves/ifood/kubernetes/ReactiveGreetingResourceTest.java
@@ -0,0 +1,21 @@
+package com.github.vitormbgoncalves.ifood.kubernetes;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class ReactiveGreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello-resteasy-reactive")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy Reactive v.2"));
+    }
+
+}
